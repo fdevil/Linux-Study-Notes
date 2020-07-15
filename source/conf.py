8117534a44d2695fe1af9088eeaf8017e77c3311
fdevil <fdevilpublic@163.com>
2020-07-15 17:47:06 +0800

    text

--- source/conf.py
+++ source/conf.py
@@ -55,4 +55,4 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-master_doc = 'index'
+
